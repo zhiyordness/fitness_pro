8cb5e484d56e1da2b75dec6f5b67e41b3e2a9afc
Zhivomir Yordanov <zhivomiryordanov@gmail.com>
2026-03-26 17:37:40 +0200

    Add azure_settings that inherits from base settings

---
+++ fitness_pro/azure_settings.py
@@ -0,0 +1,59 @@
+
+import os
+from .settings import *
+
+
+DEBUG = False
+
+
+ALLOWED_HOSTS = [
+    'fitnesspro.azurewebsites.net',
+    'fitnesspro-arhfd9ftcng6dbcr.polandcentral-01.azurewebsites.net',
+    'localhost',
+    '127.0.0.1',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://fitnesspro.azurewebsites.net',
+    'https://fitnesspro-arhfd9ftcng6dbcr.polandcentral-01.azurewebsites.net',
+    'http://localhost:8000',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'fitnesspro_db'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASS'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+        'CONN_MAX_AGE': 600,
+        'OPTIONS': {
+            'sslmode': os.getenv('DBSSLMODE', 'require'),
+        },
+    }
+}
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'common.middleware.RateLimitMiddleware',
+]
+
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
